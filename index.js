aab7f383986ddbe03507a9baf518ceab35550d3a
mcMineyC <jedi.master@falconhosting.net>
2024-11-04 09:57:35 -0800

    make port an optional env variable

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const fs = require("fs");
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 const markdownUtils = require("./markdown-utils");
 const updateUtils = require("./update-utils");
 const renderer = require("./rendering");
